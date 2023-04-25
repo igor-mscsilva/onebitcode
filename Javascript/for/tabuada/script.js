dcd188ce47b47da08076d1844f4591b5cf516d82
igor Silva <igor_mscsilva@outlook.com>
2023-04-25 10:57:28 -0300

    Tabuada em js
    Exercício em js e tabuada em js

---
+++ Javascript/for/tabuada/script.js
@@ -0,0 +1,13 @@
+let numero = prompt("Informe um numero para calcular o valor da tabuada: [1 a 20]")
+numero = parseFloat(numero)
+
+for(let i = 0 ; i <= 10; i++){
+    alert(`Os valores da tabuada do número ${numero} é :
+
+    \n${numero} x ${i} = ${numero * i}`)
+}
+
+for (var i = 1; i <= 10; i++) {  
+    // Números de 1 a 10
+    console.log("5 x " + i + " = " + (5 * i));
+}
