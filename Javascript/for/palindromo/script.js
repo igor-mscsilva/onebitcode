b4f110153d1636015e425607f946504f58a37ef2
igor Silva <igor_mscsilva@outlook.com>
2023-05-05 17:58:00 -0300

    Palindromo e tabuada
    Foi feito alguns exercícios utilizando o js e feito algumas anotações.

---
+++ Javascript/for/palindromo/script.js
@@ -0,0 +1,16 @@
+const word = prompt("Digite um palavra, para consultarmos se ela é (PALINDROMO)")
+let wordbackwards = ""
+
+//mostrar a palavra ao contrario
+for(let i = word.length - 1; i >= 0; i--){
+    wordbackwards += word[i]
+}
+
+//Verificar se a palavra ao contrario é igual a palavra digitada e mostrar o resultado.
+
+if(word === wordbackwards){
+    alert(word + " é uma palíndromo !" )
+}else{
+    alert(word + " não é um palíndromo! \n\n"   + " Palavra ao contrario: " + word + " !== " + wordbackwards)
+}
+
--- Javascript/for/tabuada/script.js
+++ Javascript/for/tabuada/script.js
@@ -1,13 +1,10 @@
 let numero = prompt("Informe um numero para calcular o valor da tabuada: [1 a 20]")
 numero = parseFloat(numero)
 
+let resultado = ""
+
 for(let i = 0 ; i <= 10; i++){
-    alert(`Os valores da tabuada do número ${numero} é :
-
-    \n${numero} x ${i} = ${numero * i}`)
+    resultado += numero + " * " + i + " = " + (numero * i) + "\n"
 }
 
-for (var i = 1; i <= 10; i++) {  
-    // Números de 1 a 10
-    console.log("5 x " + i + " = " + (5 * i));
-}
+alert ("Resultado da tabuada de " + numero + "\n\n" + resultado)
