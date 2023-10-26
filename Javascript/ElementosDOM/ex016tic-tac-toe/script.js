49533387a4628d58a752f8440a399307fa1fb849
Igor Silva <igor_mscsilva@outlook.com>
2023-10-25 23:21:01 -0300

    Jogo da velha
    Criado os elementos da pagina com HTML, CSS e JS

---
+++ Javascript/ElementosDOM/ex016tic-tac-toe/script.js
@@ -0,0 +1,15 @@
+// selecionar o id e dentro dele as tags span
+const boardRegions = document.querySelectorAll('#gameBoard span')
+// Criar uma tabuleiro virtual
+let vBoard = []
+let turnPlayer = ''
+
+// Mostrar o nome do jogador da vez
+function updateTitle(){
+    const playerInput = document.getElementById(turnPlayer)
+    document.getElementById('turnPlayer').innerText = playerInput.value
+}
+
+function initializeGame(){
+    vBoard = [['', '',''], ['','',''],['','','']]
+}
